4db8cb11afd10e2213b40160f7ac1ad73ed3a473
Kseniya <103533295+KseniyaEfimova@users.noreply.github.com>
2024-03-17 01:46:03 +0300

    Update mainPageScript.js

--- mainPageScript.js
+++ mainPageScript.js
@@ -65,7 +65,7 @@ function openModal(e) {
   const modalWindow = document.querySelector(e.target.dataset.modalTarget);
   if (modalWindow == null) return;
   if (e.target.modalTarget === '#login') {
-    alert('используйте данные из файла "README.md"');
+    alert('используйте данные из файла "README.md. Пример : login Tom Colt, pin 1111"');
   }
   modalWindow.classList.add('active');
   overlay.classList.add('active');
